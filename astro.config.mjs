dafd89266739a791d572a9a60f53d62365d2ec34
mtthwbrdly <85289691+mtthwbrdly@users.noreply.github.com>
2026-02-24 16:48:49 +0200

    Update to Sanity Studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), sanity()]
 });
