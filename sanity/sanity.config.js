0ad9123b833fb5fa4776f2ee73472b1b3f4091eb
mtthwbrdly <85289691+mtthwbrdly@users.noreply.github.com>
2026-02-24 17:07:18 +0200

    Add React integration for Sanity Studio

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -1,21 +1,24 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemaTypes";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Matthew Bradley Website',
+  name: "default",
+  title: "Matthew Bradley Website",
 
-  projectId: 'icj9mmoe',
-  dataset: 'production',
+  projectId: "icj9mmoe",
+  dataset: "production",
+  apiVersion: "2025-01-01",
 
   basePath: "/studio",
 
-
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(),
+    process.env.NODE_ENV === "development" && visionTool(),
+  ].filter(Boolean),
 
   schema: {
     types: schemaTypes,
   },
-})
+});
